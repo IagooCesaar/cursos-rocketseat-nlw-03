b7ce6a3f782c61dff57ac4e676f479a484dd90de
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-10-15 18:12:43 -0300

    Handle with exceptions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,10 @@
 import express, { json } from 'express'
+import 'express-async-errors';
+import errorHandler from './errors/handler';
+
+import './database/connection'
 import routes from './routes';
 import path from 'path'
-import './database/connection'
 
 const app = express();
 app.use(express.json());
@@ -9,5 +12,6 @@ app.use(routes)
 app.use('/uploads', express.static(
   path.join(__dirname, '..', 'uploads')
 ))
+app.use(errorHandler)
 
 app.listen(3333)
