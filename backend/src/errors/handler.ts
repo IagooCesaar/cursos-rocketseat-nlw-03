86b5006e4e4df6ed2d3d446625ce41ce2f9dcb28
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-10-15 18:25:40 -0300

    validating errors

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,4 +1,9 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
+
+interface ValidationErrors {
+  [key: string]: string[];
+}
 
 const errorHandler: ErrorRequestHandler = (
   error,
@@ -6,7 +11,19 @@ const errorHandler: ErrorRequestHandler = (
   response,
   next
 ) => {
-  console.error(error)
+  console.error(error);
+  if (error instanceof ValidationError) {
+    let errors: ValidationErrors = {};
+    error.inner.forEach(err => {
+      errors[err.path] = err.errors;
+    });
+    return response.status(400).json({
+      message: 'Validation fails',
+      errors
+    })
+
+  }
+
   return response.status(500).json({
     message: 'Internal server error'
   })
