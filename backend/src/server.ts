cc5853c6af2e1bca0ee1dd2db97ca3a8c1d114b8
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2020-10-15 14:35:40 -0300

    Connecting into BD and create first orphanage

---
+++ backend/src/server.ts
@@ -0,0 +1,36 @@
+import express, { json } from 'express'
+import { getRepository } from 'typeorm'
+import Orphanage from './models/orphanges'
+
+import './database/connection'
+
+const app = express();
+app.use(express.json());
+
+app.post('/orphanages', async (req, res) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions, 
+    opening_hours,
+    open_on_weekends,
+  } = req.body;
+  const orphanagesRepository = getRepository(Orphanage);
+
+  const orphanage = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions, 
+    opening_hours,
+    open_on_weekends, 
+  });
+  await orphanagesRepository.save(orphanage)
+
+  return res.status(201).json(orphanage)
+})
+
+app.listen(3333)
